a4953c45c00b0ea5ee2947aa6056abb68740643b
Peter Stein <peterstein@protonmail.com>
2019-02-08 19:48:19 +0100

    Reduce redundancy in get_runs and get_fills

--- wbmcrawlr/__init__.py
+++ wbmcrawlr/__init__.py
@@ -17,4 +17,4 @@ from __future__ import absolute_import
 from future import standard_library
 
 standard_library.install_aliases()
-__version__ = "0.1.3"
+__version__ = "0.2.0"
--- wbmcrawlr/oms.py
+++ wbmcrawlr/oms.py
@@ -62,83 +62,60 @@ def get_fill(fill_number):
     return get_resource("fills", parameters)
 
 
-def _get_runs(begin, end, cookies, page=0):
-    parameters = (
-        "page[offset]={page}&page[limit]={page_size}&"
-        "&filter[run_number][GE]={begin}"
-        "&filter[run_number][LE]={end}"
-        "&filter[sequence][EQ]=GLOBAL-RUN"
-        "&sort=run_number"
-    ).format(begin=begin, end=end, page=page * PAGE_SIZE, page_size=PAGE_SIZE)
+def _get_resources(table, parameters, cookies, page=0):
+    parameters = "page[offset]={page}&page[limit]={page_size}&{parameters}".format(
+        page=page * PAGE_SIZE, page_size=PAGE_SIZE, parameters=parameters
+    )
 
-    url = "{base}{resource}?{parameters}".format(
-        base=OMS_API_URL, resource="runs", parameters=parameters
+    url = "{base}{table}?{parameters}".format(
+        base=OMS_API_URL, table=table, parameters=parameters
     )
 
     response = cernrequests.get(url, cookies=cookies)
     return response.json()
+
+
+def get_resources(table, parameters):
+    cookies = get_sso_cookies("https://cmsoms.cern.ch")
+
+    response = _get_resources(table, parameters, cookies)
+    resource_count = response["meta"]["totalResourceCount"]
+    page_count = math.ceil(resource_count / PAGE_SIZE)
+
+    print("Total number of {}: {}".format(table, resource_count))
+    print()
+
+    resources = []
+    for resource in response["data"]:
+        resources.append(flatten_json_response(resource))
+
+    for page in range(1, page_count + 1):
+        print_progress(page, page_count, text="Page {}/{}".format(page, page_count))
+        response = _get_resources(table, parameters, cookies, page)
+        for resource in response["data"]:
+            resources.append(flatten_json_response(resource))
+    print()
+    print()
+    return resources
 
 
 def get_runs(begin, end):
     print("Getting runs {} - {} from CMS OMS".format(begin, end))
-    cookies = get_sso_cookies("https://cmsoms.cern.ch")
-    response = _get_runs(begin, end, cookies)
-    run_count = response["meta"]["totalResourceCount"]
-    page_count = math.ceil(run_count / PAGE_SIZE)
-
-    print("Total number of runs: {}".format(run_count))
-    print()
-
-    runs = []
-    for run in response["data"]:
-        runs.append(flatten_json_response(run))
-
-    for page in range(1, page_count + 1):
-        print_progress(page, page_count, text="Page {}/{}".format(page, page_count))
-        response = _get_runs(begin, end, cookies, page)
-        for run in response["data"]:
-            runs.append(flatten_json_response(run))
-    print()
-    print()
-    return runs
-
-
-def _get_fills(begin, end, cookies, page=0):
     parameters = (
-        "page[offset]={page}&page[limit]={page_size}&"
-        "&filter[fill_number][GE]={begin}"
-        "&filter[fill_number][LE]={end}"
-        "&filter[start_stable_beam][NEQ]=null"
-        "&sort=fill_number"
-    ).format(begin=begin, end=end, page=page * PAGE_SIZE, page_size=PAGE_SIZE)
-
-    url = "{base}{resource}?{parameters}".format(
-        base=OMS_API_URL, resource="fills", parameters=parameters
-    )
-
-    response = cernrequests.get(url, cookies=cookies)
-    return response.json()
+        "filter[run_number][GE]={begin}"
+        "&filter[run_number][LE]={end}"
+        "&filter[sequence][EQ]=GLOBAL-RUN"
+        "&sort=run_number"
+    ).format(begin=begin, end=end)
+    return get_resources("runs", parameters)
 
 
 def get_fills(begin, end):
     print("Getting fills {} - {} from CMS OMS".format(begin, end))
-    cookies = get_sso_cookies("https://cmsoms.cern.ch")
-    response = _get_fills(begin, end, cookies)
-    fill_count = response["meta"]["totalResourceCount"]
-    page_count = math.ceil(fill_count / PAGE_SIZE)
-
-    print("Total number of fills: {}".format(fill_count))
-    print()
-
-    fills = []
-    for fill in response["data"]:
-        fills.append(flatten_json_response(fill))
-
-    for page in range(1, page_count + 1):
-        print_progress(page, page_count, text="Page {}/{}".format(page, page_count))
-        response = _get_fills(begin, end, cookies, page)
-        for fill in response["data"]:
-            fills.append(flatten_json_response(fill))
-    print()
-    print()
-    return fills
+    parameters = (
+        "filter[fill_number][GE]={begin}"
+        "&filter[fill_number][LE]={end}"
+        "&filter[start_stable_beam][NEQ]=null"
+        "&sort=fill_number"
+    ).format(begin=begin, end=end)
+    return get_resources("fills", parameters)
