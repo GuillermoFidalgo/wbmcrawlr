42c1c491e8352b3fef9d4eb9918da10cf3b923f4
VasviS <vasvisharma@gmail.com>
2020-07-06 10:13:59 +0200

    Declare certificate path

--- wbmcrawlr/constants.py
+++ wbmcrawlr/constants.py
@@ -1,4 +1,4 @@
 TIMEOUT_TIME = 0.500
-staging_cert = "private/usercert.pem"
-staging_key = "private/userkey.pem"
+staging_cert = "/eos/home-t/tkdqmdoc/private/usercert.pem"
+staging_key = "/eos/home-t/tkdqmdoc/private/userkey.pem"
 CERT_TUPLE = (staging_cert, staging_key)
